1baaba3ed1dc81b462d955334539cb41c28a529e
Rahvalod <dev@mac>
2026-04-05 09:52:12 +0200

    Removed Subscription check from EU and Courts searches

--- Baza Prawna/Baza Prawna/Searches/Acts_EU/Search_EU.swift
+++ Baza Prawna/Baza Prawna/Searches/Acts_EU/Search_EU.swift
@@ -173,7 +173,7 @@ struct SearchEU_View: View, SearchResettable {
                             },
                             hideResultsOnTap: true,
                             showingResults: $showingResults,
-                            searchPremiumCheck: { PaywallManager.shared.checkPremiumAccess() },
+                            searchPremiumCheck: nil, // No premium check for search (free feature)
                             alertPremiumCheck: { PaywallManager.shared.checkPremiumAccess() }
                         )
                         
--- Baza Prawna/Baza Prawna/Searches/Court_Administrative/Search_CourtNSA.swift
+++ Baza Prawna/Baza Prawna/Searches/Court_Administrative/Search_CourtNSA.swift
@@ -159,7 +159,7 @@ struct Search_CourtNSA_View: View, SearchResettable {
                             },
                             hideResultsOnTap: true,
                             showingResults: $showingResults,
-                            searchPremiumCheck: { PaywallManager.shared.checkPremiumAccess() },
+                            searchPremiumCheck: nil, // No premium check for search (free feature)
                             alertPremiumCheck: { PaywallManager.shared.checkPremiumAccess() }
                         )
                         
